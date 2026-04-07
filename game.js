588277223f0330a6a4a03423512fcdaab36c4b18
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-04-07 10:53:16 -0700

    Finished button logic

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
+let score = 0;
+let pointsPerClick = 1;
 
+const scoreRef = document.querySelector('#score-display');
+const rateRef = document.querySelector('#rate-display');
+const button = document.querySelector('#click-btn');
+
+function updateDisplay() {
+    scoreRef.textContent = `Score: ${score}`;
+    rateRef.textContent = `Points per click: ${pointsPerClick}`;
+};
+
+button.addEventListener("click", function() {
+    score = score + pointsPerClick;
+    updateDisplay();
+});
